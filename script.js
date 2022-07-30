40b27147ee802efb61dd5ee2e541da318f949da1
sivamoorthy4 <mahadhaisiva@gmail.com>
2022-07-30 22:57:31 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //Sample
+
+class People {
+
+    constructor(name,age,gender){
+
+        setName (name);
+        setAge (age);
+        setGender(gender);
+
+    }
+
+     function setAge (age) {
+
+        this.age=age;
+    }
+
+    setName (name) {
+
+        this.name=name;
+    }
+
+    setGender (gender) {
+
+        this.gender=gender;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    getGender() {
+        return this.gender;
+    }
+
+}
+
+let p1 = new People("shiva",27,"male");
+
+console.log(p1.getName());
+
+
+
+
+
+
